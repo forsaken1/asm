04a9c08231e1e10bcd3f13c0c4b13d59d5804381
forsaken1 <alexey2142@mail.ru>
2012-11-08 01:09:39 +1000

    fuuuu

--- hw2/ex2.asm
+++ hw2/ex2.asm
@@ -6,6 +6,7 @@ include \masm32\include\masm32rt.inc
 .data
     fi      db  '%d', 0
     fo      db  '%d', 0
+    fon     db  '%d', 13, 10, 0
     nextstr db  13, 10
     msg     db  'Enter number:', 13, 10, 0
     v       db  100 dup (0)
@@ -21,18 +22,18 @@ start:
     invoke  crt_printf, addr msg
     invoke  crt_scanf, addr fi, addr n
     
-cycle:
+Cycle:
     mov     eax, 0
     mov     al, [i]
-    cmp     [n], al
-    je      endcycle
+    cmp     al, [n]
+    je      EndCycle
     
     mov     v[eax], al
     inc     i
-    jmp     cycle
+    jmp     Cycle
 
-endcycle:
-    push    0
+EndCycle:
+    push    dword ptr 0
     call    lex
     
     inkey
@@ -40,53 +41,127 @@ endcycle:
 
 lex proc
     mov     ebx, 0
-    pop     ebx
-    cmp     ebx, dword ptr [n]
-    jne     IfFalse
+    mov     ebx, [esp+4]
+    cmp     bl, [n]
 
-    mov     ecx, 0
+comment *
+    mov     edx, 0
+    mov     edx, [esp]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+4]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+8]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+12]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+16]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+20]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+24]
+    invoke  crt_printf, addr fon, edx    
+    
+    inkey *
+    
+    jl      IfFalse
+
+    mov     [i], 0
 
 Print:
-    mov     eax, 0
-    mov     eax, dword ptr p[ecx]
-    mov     eax, dword ptr v[eax]
-    add     eax, 1
-    invoke  crt_printf, addr fo, eax
-    inc     ecx
-    cmp     ecx, dword ptr [n]
-    jne     Print
+    mov     ecx, 0
+    mov     cl, [i]
+    cmp     cl, [n]
+    je      PrintEnd
 
+    mov     eax, 0
+    mov     al, p[ecx]
+    mov     al, v[eax]
+    add     al, 1
+    invoke  crt_printf, addr fo, eax
+    inc     i
+    jmp     Print
+
+PrintEnd:
     invoke  crt_printf, addr nextstr
-    ret
+    jmp     ForEnd
 
 IfFalse:
-    mov     ecx, 0
+    mov     [i], 0
     
 ForBegin:
-    cmp     ecx, dword ptr [n]
-    je      ForEnd
+    mov     ecx, 0
+    mov     cl, [i]
+    cmp     cl, [n]
+    jge     ForEnd
 
     mov     eax, 0
-    mov     eax, dword ptr used[ecx]
-    cmp     eax, 1
+    mov     al, used[ecx]
+    cmp     al, 1
     je      UsedTrue
 
     mov     used[ecx], 1
-    mov     dword ptr p[ebx], ecx
-    mov     edx, 0
-    mov     edx, ebx
-    add     edx, 1
-    push    edx
+    mov     p[ebx], cl
+    add     bl, 1
+    push    ecx
+    push    ebx
     call    lex
+    mov     ebx, 0
+    mov     ecx, 0
+
+    ;comment *
+    mov     edx, 0
+    mov     edx, [esp]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+4]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+8]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+12]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+16]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+20]
+    invoke  crt_printf, addr fon, edx
+
+    mov     edx, 0
+    mov     edx, [esp+24]
+    invoke  crt_printf, addr fon, edx    
+    
+    inkey 
+
+    pop     ebx
+    pop     ecx
     mov     p[ebx], 0
     mov     used[ecx], 0
 
 UsedTrue:
-    inc     ecx
+    inc     i
     jmp     ForBegin
 
 ForEnd:
-    ret
+    ret    
 
 lex endp
 
